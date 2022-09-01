88c72f3c28d1547e5fbd93eb5017b71e60ad93e5
Daria-Russia <111522488+Daria-Russia@users.noreply.github.com>
2022-09-01 10:01:41 +0300

    Task4 fixed and committed

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -4,14 +4,14 @@ Console.Write("Insert the first number: ");
 int A = Convert.ToInt32(Console.ReadLine()); 
 Console.Write("Insert the second number: ");
 int B = Convert.ToInt32(Console.ReadLine());
-Console.Write("Insert the second number: ");
+Console.Write("Insert the third number: ");
 int C = Convert.ToInt32(Console.ReadLine());
 if (A>C && A>B)
 Console.WriteLine("Max number "+A);
 else if (B>C)
-Console.WriteLine("Max number"+B);
+Console.WriteLine("Max number "+B);
 else
-Console.WriteLine("Max number"+C);
+Console.WriteLine("Max number "+C);
 
 
 
